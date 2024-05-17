b5427bc4933c2a057d020eb8692d83b133cf2705
umayanaganacooray <ume.cooray@gmail.com>
2024-05-17 06:26:20 +0530

    adding todo app

---
+++ controllers/todo.js
@@ -0,0 +1,55 @@
+const moment = require("moment");
+const Todo = require("../models/Todo");
+
+const homeController = async(req,res,next)=>{
+    try{
+        const todos = await Todo.find({}).sort({createdAt: -1});
+        res.locals.moment = moment;
+        res.render("index",{title:"List todo",todos});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const addTodoFormController = (req,res,next)=>{
+    try{
+        res.render("newTodo",{title:"New todo"});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const updateTodoFormController = (req,res,next)=>{
+    try{
+        res.render("updateTodo",{title:"Update todo"});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const deleteTodoFormController = (req,res,next)=>{
+    try{
+        res.render("deleteTodo",{title:"Delete todo"});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const addTodoController = async (req,res,next)=>{
+    try{
+        const {title, desc}=req.body;
+        const newTodo = new Todo({title,desc});
+        await newTodo.save();
+        res.redirect("/");
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+module.exports = {
+    homeController, 
+    addTodoFormController, 
+    updateTodoFormController, 
+    deleteTodoFormController,
+    addTodoController,
+};
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const express = require("express");
-
+const app = require("./app");
 const PORT = 8000;
-
-// init app
-const app = express();
-
-// view engine
-app.set("view engine","ejs");
 
 // listen server
 app.listen(PORT, ()=> {
